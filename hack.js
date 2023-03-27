1d680cb6391c91daf05a3f68ea07951baa77cbcc
Ryan McQuen <rpcm@linux.com>
2023-03-26 22:54:47 -0700

    Call me Whizzy.
    Signed-off-by: Ryan McQuen <rpcm@linux.com>

--- hack.js
+++ hack.js
@@ -4,10 +4,11 @@
             "MWK key logo v2 HQ",
             "MWK key logo v2 HQ - Whiz on kid",
             "MWK key logo v2 HQ - Who you kiddin'",
+            "MWK key logo v2 HQ - Call me Whizzy",
         ];
         const logo = document.querySelector(".logo");
         logo.addEventListener("click", () => {
-            logo.src = `assets/${logos[Math.floor(Math.random() * 3)]}.png`;
+            logo.src = `assets/${logos[Math.floor(Math.random() * 4)]}.png`;
 
             return false;
         });
