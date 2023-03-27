a70a3e6e135ea9a9264d9abcb6679aaa2a57a697
Ryan McQuen <rpcm@linux.com>
2023-03-26 21:58:31 -0700

    Make paths valid.
    Signed-off-by: Ryan McQuen <rpcm@linux.com>

--- hack.js
+++ hack.js
@@ -1,13 +1,13 @@
 (() => {
     window.addEventListener("DOMContentLoaded", () => {
         const logos = [
-            "MWK key logo v2 HQ.png",
-            "MWK key logo v2 HQ - Whiz on kid.png",
-            "MWK key logo v2 HQ - Who you kiddin'.png",
+            "MWK key logo v2 HQ",
+            "MWK key logo v2 HQ - Whiz on kid",
+            "MWK key logo v2 HQ - Who you kiddin'",
         ];
         const logo = document.querySelector(".logo");
         logo.addEventListener("click", () => {
-            logo.src = logos[Math.floor(Math.random() * 3)];
+            logo.src = `assets/${logos[Math.floor(Math.random() * 3)]}.png`;
 
             return false;
         });
