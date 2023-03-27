7953e659d09e96f36f003df4767f52501e9e62e9
Ryan McQuen <rpcm@linux.com>
2023-03-26 21:54:57 -0700

    Randomizer.
    Signed-off-by: Ryan McQuen <rpcm@linux.com>

--- hack.js
+++ hack.js
@@ -1,13 +1,13 @@
 (() => {
     window.addEventListener("DOMContentLoaded", () => {
         const logos = [
-            "MWK key logo v2 HQ - Who you kiddin'.png",
-            "MWK key logo v2 HQ - Whiz on kid.png",
             "MWK key logo v2 HQ.png",
+            "MWK key logo v2 HQ - Whiz on kid.png",
+            "MWK key logo v2 HQ - Who you kiddin'.png",
         ];
         const logo = document.querySelector(".logo");
         logo.addEventListener("click", () => {
-            logo.src = logos[Math.random * 3];
+            logo.src = logos[Math.floor(Math.random() * 3)];
         });
     });
 })();
