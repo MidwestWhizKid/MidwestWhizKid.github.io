1a7df9fd15058e442f4e1ba15ee285b7884fef1b
Ryan McQuen <rpcm@linux.com>
2023-03-26 21:53:17 -0700

    Add DOM listener. My first listener.
    Signed-off-by: Ryan McQuen <rpcm@linux.com>

--- hack.js
+++ hack.js
@@ -1,11 +1,13 @@
 (() => {
-    const logos = [
-        "MWK key logo v2 HQ - Who you kiddin'.png",
-        "MWK key logo v2 HQ - Whiz on kid.png",
-        "MWK key logo v2 HQ.png",
-    ];
-    const logo = document.querySelector(".logo");
-    logo.addEventListener("click", () => {
-        logo.src = logos[Math.random * 3];
+    window.addEventListener("DOMContentLoaded", () => {
+        const logos = [
+            "MWK key logo v2 HQ - Who you kiddin'.png",
+            "MWK key logo v2 HQ - Whiz on kid.png",
+            "MWK key logo v2 HQ.png",
+        ];
+        const logo = document.querySelector(".logo");
+        logo.addEventListener("click", () => {
+            logo.src = logos[Math.random * 3];
+        });
     });
 })();
